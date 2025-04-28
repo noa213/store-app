6d95d5c24f32178cc4113cb3df72a193beeecb60
Your Name <your-email@example.com>
2025-04-28 11:55:00 +0300

    Creating basic functions for drink

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\DrinkController;
 
 Route::prefix('users')->group(function () {
     Route::post('/', [UserController::class, 'store']);    
@@ -10,4 +11,13 @@ Route::prefix('users')->group(function () {
     Route::get('/{id}', [UserController::class, 'show']);   
     Route::put('/{id}', [UserController::class, 'update']); 
     Route::delete('/{id}', [UserController::class, 'destroy']); 
+});
+
+Route::prefix('drinks')->group(function () {
+    Route::post('/', [DrinkController::class, 'store']);    
+    Route::get('/', [DrinkController::class, 'index']);       
+    Route::get('/{id}', [DrinkController::class, 'show']);   
+    Route::put('/{id}', [DrinkController::class, 'update']); 
+    Route::delete('/{id}', [DrinkController::class, 'destroy']); 
+    Route::get('/user/{userId}', [DrinkController::class, 'getByUserId']);
 });
