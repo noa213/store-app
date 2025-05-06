021e4bed7a2d530854d1d0a0762beb10f04c4a9c
ahuva12 <ahuvareshit@gmail.com>
2025-05-06 14:56:54 +0300

    display the categories table

---
+++ frontend/src/app/CategoriesPage/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CategoriesTable from "@/components/categories/CategoriesTable";
+
+const CategoriesPage = () => {
+  return (
+    <div className="container mx-auto">
+      <CategoriesTable />
+    </div>
+  );
+};
+
+export default CategoriesPage;
