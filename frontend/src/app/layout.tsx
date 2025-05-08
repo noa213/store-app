98dfcb41b4cd5a65ce7516be3ede5d6dc6d40113
noa213 <abmagori@gmail.com>
2025-05-08 10:32:49 +0300

    add global.css

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -3,6 +3,7 @@ import DrinkTable from "@/components/drinks/DrinkTable";
 import Users from "./users/page";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import './globals.css'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
